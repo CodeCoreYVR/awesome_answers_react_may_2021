c3a74218a751e7280b985a932162e43f8b0611ce
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-08-11 10:45:24 -0700

    http requests and Hacky Sign In Session

--- awesome-answers-react-client/src/App.js
+++ awesome-answers-react-client/src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-// import QuestionShowPage from './components/QuestionShowPage';
-// import {QuestionIndexPage} from './components/QuestionIndexPage';
+import QuestionShowPage from './components/QuestionShowPage';
+import {QuestionIndexPage} from './components/QuestionIndexPage';
 import CurrentDateTime from './components/CurrentDateTime';
+import { Session } from './requests'
 
 // function App() {
 //   return (
@@ -17,8 +18,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clocksCount: [1]
+      clocksCount: [1],
+      user: null
     }
+  }
+
+  //Hacky Sign in with mock data passed as params
+  componentDidMount () {
+    Session.create({
+      email: 'js@winterfell.gov',
+      password: 'supersecret'
+    }).then(current_user => {
+      this.setState((state) => {
+        return {
+          user: current_user
+        }
+      })
+    })
   }
   render() {
     return (
