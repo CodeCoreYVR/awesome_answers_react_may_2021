f26f00afa33225d8666bad9d58b18665bb50f51b
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-08-12 11:03:43 -0700

    Add SignInPage and store currentUser in react client

--- awesome-answers-react-client/src/components/NavBar.js
+++ awesome-answers-react-client/src/components/NavBar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = (props) => {
+const NavBar = ({ currentUser }) => {
     return(
     <nav>
         <NavLink to='/questions'>Questions Index</NavLink>
         |
-        <NavLink to='/questions/new'>New Question</NavLink>
+        { currentUser ? (
+            <React.Fragment> 
+                {/* The React Fragment allows us to write components beside each other without wrapping
+                them in a parent container.  This stops us from messing with the layout and keeps the
+                Navlinks as direct children of the <nav>.  The short form is <> </> */}
+                <NavLink to='/questions/new'>New Question</NavLink>
+                -
+                <span>Welcome, {currentUser.first_name}</span>
+            </React.Fragment>
+        ) : (
+            <NavLink to='sign_in'>Sign In</NavLink>
+        )
+    }
     </nav>
     )
 }
--- awesome-answers-react-client/src/requests.js
+++ awesome-answers-react-client/src/requests.js
@@ -65,4 +65,12 @@ export const Session = {
     }
 }
 
+export const User = {
+    current() {
+        return fetch(`${BASE_URL}/users/current`, {
+            credentials: 'include'
+        }).then(res => res.json())
+    }
+}
+
 
